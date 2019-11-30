b804b17146df8c5cd556b79d2e2f6f8475546282
Daniel2 <dgoon9608>
2019-11-30 02:45:17 -0500

    Need popup component

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './Card.css'
 
 class Card extends Component{
     render(){
         // let classToApply = this.props.product.selected? "selected": "";
         let source = this.props.card.source;
         // alert(source);
-        return <img src={require(""+source)} alt ={this.props.card.source}/>
+        return <img className = "card" src={require(""+source)} alt ={this.props.card.source}/>
     }
 }
 export default Card;
